9ca024086fbc1678ffc3e5b7f7a65ebb6838af95
talkal <talkalisker@gmail.com>
2026-05-01 14:55:33 +0300

    fix: Resolve broken image paths in Hero, Process, and Gallery by mapping correct filenames

--- js/scripts.js
+++ js/scripts.js
@@ -50,16 +50,17 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Big Fish Energy',
             folder: 'Big Fish Energy',
             images: [
-                'tamar_gold2__2026-02-25T174635.000Z.jpg',
-                'tamar_gold2__2026-02-25T174635.000Z_1.jpg'
+                'tamar_gold2__2026-04-11T165221.000Z_1.jpg',
+                'tamar_gold2__2026-04-11T165221.000Z_2.jpg',
+                'tamar_gold2__2026-04-11T165221.000Z_3.jpg'
             ]
         },
         {
             name: 'Lemon Tree',
             folder: 'Lemon Tree',
             images: [
-                'tamar_gold2__2026-02-08T181232.000Z.jpg',
-                'tamar_gold2__2026-02-08T181232.000Z_1.jpg'
+                'tamar_gold2__2026-04-26T170713.000Z.jpg',
+                'tamar_gold2__2026-04-26T170713.000Z_1.jpg'
             ]
         },
         {
@@ -67,6 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
             folder: 'Yotam',
             images: [
                 'tamar_gold2__2026-03-29T170920.000Z.jpg',
+                'tamar_gold2__2026-03-29T170920.000Z_2.jpg',
+                'tamar_gold2__2026-03-29T170920.000Z_3.jpg',
+                'tamar_gold2__2026-03-29T170920.000Z_4.jpg',
                 'tamar_gold2__2026-03-29T170920.000Z_5.jpg'
             ]
         },
@@ -74,8 +78,22 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Hagar',
             folder: 'Hagar',
             images: [
-                'tamar_gold2__2026-01-14T155322.000Z.jpg',
-                'tamar_gold2__2026-03-29T170920.000Z_4.jpg'
+                'tamar_gold2__2026-04-18T180857.000Z.jpg',
+                'tamar_gold2__2026-04-18T180857.000Z_1.jpg',
+                'tamar_gold2__2026-04-18T180857.000Z_2.jpg',
+                'tamar_gold2__2026-04-18T180857.000Z_3.jpg',
+                'tamar_gold2__2026-04-18T180857.000Z_4.jpg',
+                'tamar_gold2__2026-04-18T180857.000Z_5.jpg'
+            ]
+        },
+        {
+            name: 'Yael',
+            folder: 'Yael',
+            images: [
+                'tamar_gold2__2026-04-02T165445.000Z.jpg',
+                'tamar_gold2__2026-04-02T165445.000Z_1.jpg',
+                'tamar_gold2__2026-04-02T165445.000Z_2.jpg',
+                'tamar_gold2__2026-04-02T165445.000Z_3.jpg'
             ]
         }
     ];
